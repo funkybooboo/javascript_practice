d1d2c5a6a17df15547b92830de0ea8f94a88206b
Nate Stott <nate.stott@pm.me>
2024-05-03 10:28:25 -0600

    add debug logging

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-run `nodemon index.js` to start app in dev mod
+## run
+
+run `nodemon index.js` to start app in dev mode
 run `node index.js` to start app in depoly mode
+
+## environment vaibles
+
+example use `export DEBUG=app:startup`
+
+- DEBUG=
+    - app:startup
+    - app:database
+    - app:*
+    - app:startup,app:database
+- NODE_ENV=
+    - development
+        - default
+    - production
+- app_mail_password=
+    - whatever the password is for the server
--- index.js
+++ index.js
@@ -2,22 +2,38 @@ const express = require("express");
 const Joi = require("joi");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const config = require("config");
+const startupDebugger = require("debug")("app:startup");
+const databaseDebugger = require("debug")("app:database");
 const logger = require("./logger");
 const authenticater = require("./authenticater");
 
 const app = express();
 
+// configuration
+console.log(`application name: ${config.get("name")}`);
+const mail_host_name = config.get("mail.host");
+console.log(`mail server: ${mail_host_name}`);
+//const mail_server_password = config.get("mail.password");
+
 // express middleware
 app.use(express.json()); // reqest.body
 app.use(express.urlencoded({extended: true})); // key=value&key=value
-app.use(express.static("public")); // localhost:port/readme.txt
+app.use(express.static("public")); // lets people go to localhost:port/docs.txt
 // third party middleware
 app.use(helmet()); // exta security
-app.use(morgan("tiny")); // logs http requests
+if (app.get("env") === "development") { // export NODE_ENV=production or testing or whatever but by default is development
+    app.use(morgan("tiny")); // logs http requests
+    startupDebugger("morgan enabled...");
+}
 // custom middleware
 app.use(logger);
 app.use(authenticater);
 
+// data base work
+databaseDebugger("connected to the database...");
+
+// dummy data
 const courses = [
     {id: 1, name: "course1"},
     {id: 2, name: "course2"},
@@ -31,6 +47,7 @@ function validateCourse(course) {
     return Joi.valid(course, schema);
 }
 
+// get endpoints
 app.get("/", (requestuest, responseponse) => {
     responseponse.send("Landing Page");
 });
@@ -48,6 +65,7 @@ app.get("/api/courses/:id", (request, response) => {
     response.send(course);
 });
 
+// post end points
 app.post("/api/courses", (request, response) => {
     const {error} = validateCourse(request.body);
     if (error) {
@@ -62,6 +80,7 @@ app.post("/api/courses", (request, response) => {
     response.send(course);
 });
 
+// put endpoints
 app.put("/api/courses/:id", (request, response) => {
     const course = courses.find(course => course.id === parseInt(request.params.id));
     if (!course) {
@@ -77,6 +96,7 @@ app.put("/api/courses/:id", (request, response) => {
     response.send(course);
 });
 
+// delete endpoints
 app.delete("/api/courses/:id", (request, response) => {
     const course = courses.find(course => course.id === parseInt(request.params.id));
     if (!course) {
@@ -88,6 +108,7 @@ app.delete("/api/courses/:id", (request, response) => {
     response.send(course);
 });
 
+// start listening
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
