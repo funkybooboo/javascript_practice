999fa27eb9e58ae2cecad9e429d5e0dbd55c515f
Nate Stott <nate.stott@pm.me>
2024-06-03 10:28:18 -0600

    sort with qurey params

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import {sort} from "fast-sort";
 
 interface User {
     id: number;
@@ -5,19 +7,30 @@ interface User {
     email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+    sortOrder: string;
+}
+
+const UserTable = async ({sortOrder}: Props) => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users', { next: { revalidate: 10 } });
     const users: User[] = await response.json();
+
+    const sortedUsers = sort(users).asc(sortOrder === 'email' ? user => user.email : user => user.name);
+
     return (
         <table className="table table-bordered">
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Email</th>
+                    <th>
+                        <Link href={"/users?sortOrder=name"}>Name</Link>
+                    </th>
+                    <th>
+                        <Link href={"/users?sortOrder=email"}>Email</Link>
+                    </th>
                 </tr>
             </thead>
             <tbody>
-                {users.map(user => <tr key={user.id}>
+                {sortedUsers.map(user => <tr key={user.id}>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                 </tr>)}
