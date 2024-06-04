687b796b1f90f008b913da14d85c01d935fba76e
Nate Stott <nate.stott@pm.me>
2024-06-04 09:19:30 -0600

    finish user CRUD ops

--- app/api/users/[userId]/route.tsx
+++ app/api/users/[userId]/route.tsx
@@ -24,10 +24,31 @@ export async function PUT(request: NextRequest, {params: {userId}}: Props) {
     if (!validation.success) {
         return NextResponse.json(validation.error.errors, {status: 400});
     }
-    return NextResponse.json({id: 1, name: body.name}, {status: 200});
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(userId) }
+    });
+    if (!user) {
+        return NextResponse.json({error: "User not found"}, {status: 404});
+    }
+    const updatedUser = await prisma.user.update({
+        where: { id: parseInt(userId) },
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    });
+    return NextResponse.json(updatedUser, {status: 200});
 }
 
-export function DELETE(request: NextRequest, {params: {userId}}: Props) {
-
+export async function DELETE(request: NextRequest, {params: {userId}}: Props) {
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(userId) }
+    });
+    if (!user) {
+        return NextResponse.json({error: "User not found"}, {status: 404});
+    }
+    await prisma.user.delete({
+        where: { id: parseInt(userId) }
+    });
     return NextResponse.json({}, {status: 200});
 }
