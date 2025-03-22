722bc68d99f13d78aed7e7c0c25c658a6234fd63
Nate Stott <nate.stott@pm.me>
2025-03-22 13:48:41 -0400

    positive and negitive testing

---
+++ testing/tests/core.test.js
@@ -0,0 +1,54 @@
+import { describe, it, expect } from "vitest";
+import { getCoupons, calculateDiscount } from "../src/core.js";
+
+describe('getCoupons', () => {
+    it('should return two elements', () => {
+        const coupons = getCoupons();
+
+        expect(coupons.length).toBeGreaterThan(0);
+    });
+
+    it('should return an array with valid coupon codes', () => {
+        const coupons = getCoupons();
+
+        coupons.forEach((coupon) => {
+            expect(coupon).toHaveProperty('code');
+            expect(typeof coupon.code).toBe('string'); // dont need if using typescript
+            expect(coupon.code).toBeTruthy();
+        });
+    });
+
+    it('should return an array with valid coupon discounts', () => {
+        const coupons = getCoupons();
+
+        coupons.forEach((coupon) => {
+            expect(coupon).toHaveProperty('discount');
+            expect(typeof coupon.discount).toBe('number'); // dont need if using typescript
+            expect(coupon.discount).toBeGreaterThan(0);
+            expect(coupon.discount).toBeLessThan(1);
+        });
+    });
+});
+
+describe('calculateDiscount', () => {
+    it('should return the discounted price if given a valid code', () => { // positive test
+        expect(calculateDiscount(10, 'SAVE10')).toBe(9);
+        expect(calculateDiscount(10, 'SAVE20')).toBe(8);
+    });
+
+    it('should handle non-numeric price', () => { // negative test
+        expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i);
+    });
+
+    it('should handle negative price', () => { // negative test
+        expect(calculateDiscount(-10, 'SAVE10')).toMatch(/invalid/i);
+    });
+
+    it('should handle non-string discount code', () => { // negative test
+        expect(calculateDiscount(10, 10)).toMatch(/invalid/i);
+    });
+
+    it('should handle a invalid discount code', () => { // negative test
+        expect(calculateDiscount(10, 'INVALID')).toBe(10);
+    });
+});
