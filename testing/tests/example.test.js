dd449a23d988e6215a32c5dd106c50560f99f8fc
Nate Stott <nate.stott@pm.me>
2025-03-22 13:17:42 -0400

    spelling

--- testing/tests/example.test.js
+++ testing/tests/example.test.js
@@ -11,7 +11,7 @@ describe('test suite', () => {
         const r = 'The requested file was not found.';
         // expect(r).toBeDefined(); // Loose (too general)
         // expect(r).toBe('The requested file was not found.'); // Tight (too specific) i.e., what if we remove the period
-        expect(r).toMatch('not found'); // Better assertion we at least need to say not found if a file is not found. That keyword is not likely to change.
+        expect(r).toMatch('not found'); // Better assertion we at least need to say not found if a file is not found. That keyword is not likely to change
     });
 
     it('test case', () => {
@@ -19,14 +19,14 @@ describe('test suite', () => {
         // expect(r).toBeDefined(); // Loose
         // expect(r).toEqual([1, 2, 3]); // Tight we don't want to be order-dependent
         // expect(r).toEqual(expect.arrayContaining([1, 2, 3])); // Better as now order does not matter
-        expect(r.length).toBeGreaterThan(0); // Maybe loose or could be good, depends on your use case!
-        expect(r).toHaveLength(3); // Maybe tight or could be good, depends on your use case!
+        expect(r.length).toBeGreaterThan(0); // Maybe loose or could be good, depends on your use case
+        expect(r).toHaveLength(3); // Maybe tight or could be good, depends on your use case
     });
 
     it('test case', () => {
         const r = { name: 'Nate' };
         // expect(r).toEqual({ name: 'Nate' }); // Tight what if we add an id property to r
-        // expect(r).toMatchObject({ name: 'Nate' }); // match on a subset of attributes. this can be night tho as we are making an assertion on the value
+        // expect(r).toMatchObject({ name: 'Nate' }); // match on a subset of attributes. This can be tight tho as we are making an assertion on the value
         expect(r).toHaveProperty('name');
     });
 });
